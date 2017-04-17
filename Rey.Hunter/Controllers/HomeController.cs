70436f69963b380b24747a4b26b8dda92f83d24f
Kevin Cai <caizhifei2003@163.com>
2017-04-17 15:27:16 +0800

    添加版本号的显示

--- Rey.Hunter/Controllers/HomeController.cs
+++ Rey.Hunter/Controllers/HomeController.cs
@@ -1,19 +1,16 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Rey.Hunter.Models.Web.Business;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Rey.Hunter.Controllers {
     [Authorize]
     public class HomeController : ReyController {
         public IActionResult Index() {
-            var db = this.GetMonDatabase();
-            var talent = db.GetCollection<Talent>().FindOne(x => true);
-
-            return View(talent);
+            return View();
         }
     }
 }

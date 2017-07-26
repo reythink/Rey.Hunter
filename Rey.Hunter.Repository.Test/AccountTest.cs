e4cffc9bf5972499777892bb38b298f68df9f1ce
Kevin Cai <caizhifei2003@163.com>
2017-07-26 13:09:52 +0800

    .

---
+++ Rey.Hunter.Repository.Test/AccountTest.cs
@@ -0,0 +1,19 @@
+﻿using Rey.Hunter.Models2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Rey.Hunter.Repository.Test {
+    public class AccountTest {
+        [Fact]
+        public void Test() {
+            var mgr = new RepositoryManager();
+            var model = new Account() {
+                Company = "Reythink",
+                Enabled = true
+            };
+            mgr.Account().InsertOne(model);
+        }
+    }
+}
--- Rey.Hunter.Repository/IRepositoryManager.cs
+++ Rey.Hunter.Repository/IRepositoryManager.cs
@@ -1,4 +1,5 @@
-﻿using Rey.Hunter.Models2;
+﻿using MongoDB.Driver;
+using Rey.Hunter.Models2;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,18 +11,34 @@ namespace Rey.Hunter.Repository {
     }
 
     public interface IRepository<TModel> {
-
+        void InsertOne(TModel model);
     }
 
     public interface IAccountRepository : IRepository<Account> {
 
     }
 
-    public class AccountRepository : IAccountRepository {
+    public abstract class RepositoryBase<TModel> : IRepository<TModel> {
+        public void InsertOne(TModel model) {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class AccountRepository : RepositoryBase<Account>, IAccountRepository {
 
     }
 
     public class RepositoryManager : IRepositoryManager {
+        private IMongoClient Client { get; }
+
+        public RepositoryManager() {
+            var settings = new MongoClientSettings() {
+                Credentials = new List<MongoCredential> { MongoCredential.CreateCredential("admin", "admin", "admin123") }
+            };
+
+            this.Client = new MongoClient(settings);
+        }
+
         public IAccountRepository Account() {
             return new AccountRepository();
         }
